2f759511e1fcf8c0a764c9aee5d044ba5e0805a2
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-20 09:36:46 -0600

    First MySQL exercise completed. Building columns with different datatypes.

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    arist VARCHAR(50),
+    name  VARCHAR(100) NOT NULL,
+    release_date INT UNSIGNED,
+    sales FLOAT,
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
+);
