c6300d104c3766b74085a296e9681cada61b42d9
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-20 10:32:08 -0600

    Completed first step in exercise(multiple insert statements).

---
+++ albums_seeder.sql
@@ -0,0 +1,363 @@
+USE codeup_test_db;
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Guns N\' Roses',
+	'Appetite for Destruction',
+	1987,
+	21.3,
+	'Heavy metal'
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Santana',
+	'Supernatural',
+	1999,
+	20.5,
+	'Rock'
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Pink Floyd',
+	'The Wall',
+	1979,
+	17.6,
+	'Progressive rock'
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Nirvana',
+	'Nevermind',
+	1991,
+	16.7,
+	'Grunge, alternative rock'
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Michael Jackson',
+	'Dangerous',
+	1991,
+	16.3,
+	'Rock / Funk / Pop'
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Michael Jackson',
+	'Bad',
+	1987,
+	18.4,
+	'Pop, funk, rock'
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Metallica',
+	'Metallica',
+	1991,
+	19.9,
+	'Thrash metal / heavy metal'
+);
+
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Shania Twain', 
+	'Come On Over', 
+	1997, 
+	29.6,
+	'Country, pop'
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Led Zeppelin', 
+	'Led Zeppelin IV', 
+	1971, 
+	29.0,
+	'Hard rock, heavy metal'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Spice Girls', 
+	'Spice',
+	1996, 
+	28,
+	'Pop'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Alanis Morissette', 
+	'Jagged Little Pill',
+	1995, 
+	24.8,
+	'Alternative rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Celine Dion', 
+	'Falling into You',
+	1996, 
+	20.2,
+	'Pop / Soft rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'The Beatles', 
+	'Sgt. Pepper\'s Lonely Hearts Club Band',
+	1967,	
+	13.1,
+	'Rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Eagles Hotel', 
+	'California',
+	1976, 
+	21.5,
+	'Rock, soft rock, folk rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Mariah Carey', 
+	'Music Box',
+	1993,	
+	19.5,
+	'Pop / R&B / Rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Various artists', 
+	'Dirty Dancing',
+	1987, 
+	17.9,
+	'Pop, rock, R&B'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Celine Dion', 
+	'Let\'s Talk About Love',
+	1997,	
+	19.3,
+	'Pop / Soft rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'The Beatles', 
+	'1',
+	2000, 
+	21.6,
+	'Rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Adele', 
+	'21',
+	2011, 
+	21.3,
+	'Pop, soul'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'The Beatles', 
+	'Abbey Road',
+	1969,	
+	14.4,
+	'Rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Bruce Springsteen', 
+	'Born in the U.S.A.',
+	1984, 
+	19.6,
+	'Rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Dire Straits', 
+	'Brothers in Arms',
+	1985, 
+	18.6,
+	'Rock'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Whitney Houston', 
+	'Whitney Houston',
+	1985, 
+	17.2,
+	'Pop, R&B'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'James Horner', 
+	'Titanic: Music from the Motion Picture',
+	1997, 
+	18.1,
+	'Soundtrack'	
+);
+INSERT INTO albums (
+    arist,
+    name,
+    release_date,
+    sales,
+    genre
+)
+VALUES (
+	'Madonna', 
+	'The Immaculate Collection',
+	1990, 
+	19.4,
+	'Pop / Dance'	
+);
+
+
+
+
+
+
+
+
+
+
+
