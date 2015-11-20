3fd8d132821b2c6bbc94cdefdf229d72132f6443
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-20 10:38:06 -0600

    Completed.

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 INSERT INTO albums (arist, name, release_date, sales, genre)
-VALUES  ('Michael Jackson', 'Thriller', 1982, 'Pop, rock, R&B', 42.4),
-        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock', 22.7),
-        ('Whitney Houston / Various artists', 'The Bodyguard', 1992, 'Soundtrack/R&B, soul, pop', 27.4),
-        ('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock, progressive rock', 20.6),
-        ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 'Rock, soft rock, folk rock', 32.2),
-        ('AC/DC', 'Back in Black', 1980, 'Hard rock', 25.9),
-        ('Bee Gees / Various artists', 'Saturday Night Fever', 1977, 'Disco', 19),
-        ('Fleetwood Mac', 'Rumours', 1977, 'Soft rock', 27.9);
+VALUES  ('Michael Jackson', 'Thriller', 1982, 42.4, 'Pop, rock, R&B'),
+        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Progressive rock'),
+        ('Whitney Houston / Various artists', 'The Bodyguard', 1992, 27.4, 'Soundtrack/R&B, soul, pop'),
+        ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard rock, progressive rock'),
+        ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 32.2, 'Rock, soft rock, folk rock'),
+        ('AC/DC', 'Back in Black', 1980, 25.9, 'Hard rock'),
+        ('Bee Gees / Various artists', 1977, 'Saturday Night Fever', 'Disco', 19),
+        ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock');
