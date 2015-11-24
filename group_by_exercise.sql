5089a706aca0964676aaa2ca90ae6443ca8db90d
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-24 09:34:44 -0600

    Completed group_by_exercise.

---
+++ group_by_exercise.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT title
+FROM titles
+GROUP BY title ASC;
+
+SELECT DISTINCT first_name, last_name 
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY first_name, last_name ASC;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
